6baf8bfabba40cf3ba88a65d7d839066360d84ec
Shubh Agrawal <shubh7185@gmail.com>
2026-02-03 00:17:22 +0530

    Adding new advance locators

--- tests/locatorspract.spec.js
+++ tests/locatorspract.spec.js
@@ -8,7 +8,12 @@ await page.goto("https://rahulshettyacademy.com/angularpractice/");
 await page.getByLabel("Check me out if you Love IceCreams!").click();
 await page.getByLabel("Employed").check();
 // for checkboxes we can use botth check() and click()
-
+await page.getByLabel("Gender").selectOption("Female");
+await page.getByPlaceholder("Password").fill("Aditi")
+await page.getByRole("button",{name:'Submit'}).click();
+await page.getByText(" The Form has been submitted successfully!.").isVisible();
+await page.getByRole("link",{name:"Shop"}).click();
+await page.locator("app-card").filter({hasText:'Nokia Edge'}).getByRole("button").click();
 
 
 
