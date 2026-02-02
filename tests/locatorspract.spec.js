e05e5ac22d3425fed01fd647f2fcce08b2f101a0
Shubh Agrawal <shubh7185@gmail.com>
2026-02-02 19:05:14 +0530

    add new test case in Locators practices

--- tests/locatorspract.spec.js
+++ tests/locatorspract.spec.js
@@ -13,3 +13,16 @@ await page.getByLabel("Employed").check();
 
 
 })
+
+
+test('Page  Playwright test', async ({page}) => { 
+
+    // if we dont want to create this playwright provide fixture page that will help to open new context and page
+    await page.goto("https://google.com");
+    // get title - assertion
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+    
+
+    
+});
