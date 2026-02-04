0c13767862946a670a667833d8b9935c714d7a7a
Shubh Agrawal <shubh7185@gmail.com>
2026-02-04 22:36:13 +0530

    Adding assertion to Calendar Automation

--- tests/calendar.spec.js
+++ tests/calendar.spec.js
@@ -5,6 +5,8 @@ test.only('Calendar Automation',async({page})=>{
 const monthNumber = "6";
 const date = "15";
 const year = "2027";
+
+const expectedList = [monthNumber,date,year]
 await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
 await page.locator(".react-date-picker__inputGroup").click();
@@ -14,7 +16,16 @@ await page.getByText(year).click();
 await page.locator(".react-calendar__tile").nth(Number(monthNumber)-1).click();
 await page.locator("//abbr[text()='"+date+"']").click();
 
+const input =  page.locator('.react-date-picker__inputGroup__input')
 
+
+for(let i=0;i<expectedList.length;i++)
+{
+    const value = await input.nth(i).inputValue();
+    expect(value).toEqual(expectedList[i]);
+
+
+}
 
 
 
