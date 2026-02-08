3d10a180a9c182aec2b1ba9bae7f696ced79c674
Shubh Agrawal <shubh7185@gmail.com>
2026-02-08 22:32:08 +0530

    handling Iframes

--- tests/MoreValidation.spec.js
+++ tests/MoreValidation.spec.js
@@ -4,9 +4,26 @@ test("More validation on hidden elements",async({page})=>
 {
     // methods for navigation
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
-    await page.goto("https://google.com");
-    await page.goBack();
-    await page.goForward();
+    // await page.goto("https://google.com");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator(".displayed-class")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator(".displayed-class")).toBeHidden();
+    // to handlde popup // dialog
+    page.on('dialog',dialog=> dialog.accept()) // it help you to listen the events
+    await page.locator("#confirmbtn").click();
+    // How we can hover to an particular element
+    await page.locator("#mousehover").hover();
+    await page.locator("a[href='#top']").click();
+    // how to handle iframe
+    const framePage=  page.frameLocator("#courses-iframe")
+    await framePage.locator("li a[href='lifetime-access']:visible").click(); // with visible tag playwright will focus on visible locator only
+    const textCehck = await framePage.locator(".text h2").textContent();
+    const output = textCehck.split(" ")[1];
+    console.log(output);
+
+
 
 
 
